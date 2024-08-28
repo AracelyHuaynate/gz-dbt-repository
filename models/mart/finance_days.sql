0b5478eb86fb5613bc6948ae2a44b841c6de9bcc
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 18:40:19 +0000

    mmm

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,12 +1,11 @@
-{{ config(
-    materialized='table') }}
+{{ config(materialized='table') }}
 SELECT
     date_date
     ,COUNT(orders_id) AS nb_transactions
     ,ROUND(SUM(revenue),0) AS revenue
     ,ROUND(AVG(revenue),1) AS average_basket
     ,ROUND(SUM(margin),0) AS operational_margin
-    ,ROUND(SUM(operational_margin),0) AS operational_margin
+    ,ROUND(SUM(operational_margin),0) AS total_operational_margin
     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
     ,ROUND(SUM(logCost),0) AS ship_cost

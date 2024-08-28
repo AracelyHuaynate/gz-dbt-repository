6e615d01ee74e62f42fd1c2ed07a1db245f0a2fc
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 21:14:39 +0000

    kk

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,8 +1,8 @@
-WITH finance_day AS (
+WITH finance_days AS (
     SELECT
         date_date,
         COUNT(orders_id) AS nb_transactions,
-        ROUND(SUM(margin), 0) AS ads_margin,  -- Definir correctamente
+        ROUND(SUM(margin), 0) AS ads_margin,
         ROUND(AVG(revenue), 1) AS average_basket,
         ROUND(SUM(operational_margin), 0) AS operational_margin,
         ROUND(SUM(purchase_cost), 0) AS purchase_cost,
@@ -19,16 +19,15 @@ int_campaigns_day AS (
         SUM(ads_cost) AS ads_cost,
         SUM(impression) AS ads_impressions,
         SUM(click) AS ads_clicks
-    FROM {{ ref('int_campaigns') }}
+    FROM {{ ref('int_campaigns') }}  -- Asegúrate de que el nombre de referencia es correcto
     GROUP BY date_date
 )
 
 SELECT
     f.date_date,
     f.nb_transactions,
-    f.ads_margin,  -- Usar el alias correcto
+    f.operational_margin,  -- Solo una referencia aquí
     f.average_basket,
-    f.operational_margin,
     f.purchase_cost,
     f.shipping_fee,
     f.ship_cost,
@@ -36,8 +35,10 @@ SELECT
     c.ads_cost,
     c.ads_impressions,
     c.ads_clicks,
-    ROUND(f.operational_margin - c.ads_cost, 0) AS calculated_ads_margin  -- Cambiar alias para evitar confusión
+    ROUND(f.operational_margin - c.ads_cost, 0) AS calculated_ads_margin 
 FROM {{ ref("finance_days") }} AS f
 LEFT JOIN {{ ref("int_campaigns_day") }} AS c
     ON f.date_date = c.date_date
 ORDER BY f.date_date DESC
+
+

13239d85367e0556d12b5caa1ab430d6c24e4279
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 18:15:40 +0000

    VV

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,15 +1,28 @@
-WITH sales_margin AS (
-  SELECT *
-  FROM {{ ref("int_sales_margin") }}
+WITH int_sales_margin AS (
+  SELECT
+    s.orders_id,
+    s.date_date,
+    s.revenue,
+    s.quantity,
+    CAST(p.purchase_price AS FLOAT64) AS purchase_price,
+    ROUND((s.quantity * CAST(p.purchase_price AS FLOAT64)), 2) AS purchase_cost,
+    ROUND((s.revenue - (s.quantity * CAST(p.purchase_price AS FLOAT64))), 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p ON s.products_id = p.products_id
 )
 
 SELECT
-  orders_id,
-  date_date,
-  SUM(margin) AS total_margin,
-  SUM(purchase_cost) AS total_purchase_cost,
-  SUM(revenue) AS total_revenue,
-  COUNT(DISTINCT products_id) AS distinct_products_count
-FROM sales_margin
-GROUP BY orders_id, date_date
-ORDER BY orders_id;
+  *
+FROM (
+  SELECT
+    orders_id,
+    MIN(date_date) AS date_date,
+    MAX(date_date) AS max_date,
+    SUM(margin) AS total_margin,
+    SUM(purchase_price) AS total_purchase_cost,
+    SUM(revenue) AS total_revenue,
+    COUNT(DISTINCT products_id) AS distinct_products_count
+  FROM {{ref("int_sales_margin")}}
+  GROUP BY orders_id
+  ORDER BY orders_id
+) AS orders_margin
--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,30 +1,25 @@
-WITH sales_margin AS (
-    -- Refiere al modelo int_sales_margin
-    SELECT *
-    FROM {{ ref("int_sales_margin") }}
-),
-shipping_info AS (
-    -- Refiere al modelo que contiene los datos de shipping y costos logísticos
-    SELECT
-        orders_id,
-        shipping_fee,
-        CAST(ship_cost AS NUMERIC) AS ship_cost,
-        logCost AS log_cost
-    FROM {{ ref("stg_raw__ship") }}
+WITH int_sales_margin AS (
+    SELECT 
+        s.orders_id,
+        s.revenue,
+        s.quantity,
+        CAST(p.purchase_price AS FLOAT64) AS purchase_price,
+        ROUND((s.quantity * CAST(p.purchase_price AS FLOAT64)), 2) AS purchase_cost,
+        ROUND(s.revenue - (s.quantity * CAST(p.purchase_price AS FLOAT64)), 2) AS margin,
+        h.shipping_fee,
+        h.log_cost,
+        h.ship_cost
+    FROM {{ref("stg_raw__sales")}} s 
+    LEFT JOIN {{ref("stg_raw__ship")}} h ON s.orders_id = h.orders_id
 )
 
-SELECT
-    sm.orders_id,
-    sm.date_date,
-    sm.total_margin,
-    sm.total_purchase_cost,
-    sm.total_revenue,
-    si.shipping_fee,
-    si.ship_cost,
-    si.log_cost,
-    -- Calcula el margen operativo
-    ROUND((sm.total_margin + si.shipping_fee - si.ship_cost - si.log_cost), 2) AS operational_margin
-FROM sales_margin sm
-LEFT JOIN shipping_info si
-ON sm.orders_id = si.orders_id
-ORDER BY sm.orders_id;
+SELECT 
+    orders_id,
+    SUM(margin) AS total_margin,
+    SUM(shipping_fee) AS total_shipping_fee,
+    SUM(log_cost) AS total_log_cost,
+    SUM(ship_cost) AS total_ship_cost,
+    SUM(margin + shipping_fee - log_cost - ship_cost) AS total_operational_margin
+FROM {{ref("int_sales_margin")}}
+GROUP BY orders_id
+ORDER BY orders_id

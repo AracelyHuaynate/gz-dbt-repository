e5c4116c0800b3657044e56c43654b8ffa66911b
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 22:21:28 +0000

    product_price

--- snapshots/products_price.sql
+++ snapshots/products_price.sql
@@ -1,15 +1,14 @@
 {% snapshot products_snapshot %}
 
--- Configuración del snapshot
-config(
-    target_schema='dbt_snapshots',  -- Esquema donde se almacenarán los snapshots
-    unique_key='products_id',       -- Clave única para identificar registros
-    strategy='check',               -- Estrategia de snapshot (puede ser 'check' o 'timestamp')
-    check_cols='all'                -- Columnas a rastrear; 'all' significa todas las columnas
-)
+{{
+    config(
+        target_schema='dbt_snapshots',
+        unique_key='products_id',
+        strategy='check',
+        check_cols='all'
+    )
+}}
 
--- Consulta para seleccionar datos del snapshot
 select * from {{ ref("stg_raw__product") }}
 
 {% endsnapshot %}
-

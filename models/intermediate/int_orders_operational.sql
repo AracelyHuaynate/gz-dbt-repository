02ad793ff5163a5bbd99c1b68d85d9895689f3a0
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 18:17:26 +0000

    VV

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,25 +1,15 @@
-WITH int_sales_margin AS (
-    SELECT 
-        s.orders_id,
-        s.revenue,
-        s.quantity,
-        CAST(p.purchase_price AS FLOAT64) AS purchase_price,
-        ROUND((s.quantity * CAST(p.purchase_price AS FLOAT64)), 2) AS purchase_cost,
-        ROUND(s.revenue - (s.quantity * CAST(p.purchase_price AS FLOAT64)), 2) AS margin,
-        h.shipping_fee,
-        h.log_cost,
-        h.ship_cost
-    FROM {{ref("stg_raw__sales")}} s 
-    LEFT JOIN {{ref("stg_raw__ship")}} h ON s.orders_id = h.orders_id
-)
-
-SELECT 
-    orders_id,
-    SUM(margin) AS total_margin,
-    SUM(shipping_fee) AS total_shipping_fee,
-    SUM(log_cost) AS total_log_cost,
-    SUM(ship_cost) AS total_ship_cost,
-    SUM(margin + shipping_fee - log_cost - ship_cost) AS total_operational_margin
-FROM {{ref("int_sales_margin")}}
-GROUP BY orders_id
-ORDER BY orders_id
+SELECT
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc

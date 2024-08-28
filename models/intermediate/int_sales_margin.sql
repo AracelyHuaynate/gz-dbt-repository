0a092f99a94cc670d2e1684400db2e540c32516d
Aracely Noemi Huaynate <aracelyhuaynateu@gmail.com>
2024-08-28 22:09:24 +0000

    ch4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,26 @@
+WITH calculated_values AS (
+    SELECT 
+        products_id, 
+        date_date, 
+        orders_id,
+        revenue, 
+        quantity, 
+        CAST(purchase_price AS FLOAT64) AS purchase_price, 
+        ROUND(s.quantity * CAST(p.purchase_price AS FLOAT64), 2) AS purchase_cost,
+        s.revenue - ROUND(s.quantity * CAST(p.purchase_price AS FLOAT64), 2) AS margin
+    FROM {{ ref("stg_raw__sales") }} s
+    LEFT JOIN {{ ref("stg_raw__product") }} p 
+        USING (products_id)
+)
+
 SELECT 
     products_id, 
     date_date, 
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64) AS purchase_price, 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p 
-    USING (products_id)
+    purchase_price,
+    purchase_cost,
+    margin,
+    {{ margin_percent('revenue', 'purchase_cost') }} AS margin_percent
+FROM calculated_values
